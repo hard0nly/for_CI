9ca67a84916d53f32f6c83656987614f0519f16e
hard0nly <52007768+hard0nly@users.noreply.github.com>
2020-01-21 01:15:23 +0300

    Update Logger.cs

--- framework/Logging/Logger.cs
+++ framework/Logging/Logger.cs
@@ -1,21 +1,23 @@
-﻿using System;
-using System.IO;
 using log4net;
 using log4net.Config;
 
-namespace TestAutomation.Logging
+namespace FrameWork
 {
-    public static class Logger
+    public class Logger
     {
-        public static ILog Log { get; } = LogManager.GetLogger("LOGGER");
+        private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
+
+        public static ILog Log
+        {
+            get
+            {
+                return log;
+            }
+        }
 
         public static void InitLogger()
         {
-            var separateIndex = AppDomain.CurrentDomain.BaseDirectory.IndexOf("bin", StringComparison.Ordinal);
-            var logConfigPath = AppDomain.CurrentDomain.BaseDirectory.Substring(0, separateIndex) +
-                             "log4net.config";
-            var logConfigFile = new FileInfo(logConfigPath);
-            XmlConfigurator.Configure(logConfigFile);
+            XmlConfigurator.Configure();
         }
     }
 }
